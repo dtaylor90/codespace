e66a49fa572bdf8549e1feb2084b5edd490443bb
dtaylor90 <37670512+dtaylor90@users.noreply.github.com>
2023-10-02 16:09:05 +0000

    Mon, Oct 2, 2023, 4:09 PM +00:00

--- parity.py
+++ parity.py
@@ -1,7 +1,16 @@
 def main():
-x = int(input( "what's x? "))
-
-if is_even (x):
+    x = int(input( "what's x? "))
+    if is_even(x):
     print( "EVEN")
 else:
     print("Odd")
+
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+    
+main()
