2659e0708d33f94dd20b21d74edf44dc1bd1a1b8
dtaylor90 <37670512+dtaylor90@users.noreply.github.com>
2023-10-02 16:46:51 +0000

    Mon, Oct 2, 2023, 4:46 PM +00:00

--- house.py
+++ house.py
@@ -1,8 +1,9 @@
 name = input("What's your name? ")
 
-if name == "Harry" or name == "Hermione" or name == "Ron":
-    print("Gryffindor")
-elif name == "Draco":
-    print("Slytherin")
-else:
-    print("Who? ")
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Draco":
+        print("Gryffindor")
+    case _:
+        print("Who? ")
